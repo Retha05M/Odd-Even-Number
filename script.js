9bc8157ad79d052a0e887e9460855ed87ebffb38
Retha05M <85956374+Retha05M@users.noreply.github.com>
2021-06-17 20:02:06 +0200

    JavaScript Code
    Odd and Even Numbers Code.

---
+++ script.js
@@ -0,0 +1,11 @@
+var userNoInput = prompt("Enter a vaild number");
+
+
+    if (userNoInput % 2===0){
+        alert(userNoInput + " is an Even number")
+    }else{
+        alert(userNoInput + " is an Odd Number");
+    }
+
+
+ 
